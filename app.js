0457b2c207dc1db55f4dfd1bc72f65711a8236ee
illur99 <86919539+illur99@users.noreply.github.com>
2021-08-19 17:26:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let openBtn=document.querySelector('.open-btn');
+let closeBtn=document.querySelector('.close-btn');
+let wrap=document.querySelectorAll('.wrap');
+
+openBtn.addEventListener('click',function(){
+    for(x=0;x<wrap.length;x++)
+    {
+        wrap[x].classList.add('visible');
+    }
+})
+
+closeBtn.addEventListener('click',function(){
+    for(x=0;x<wrap.length;x++)
+    {
+        wrap[x].classList.remove('visible');
+    }
+})
